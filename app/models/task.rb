bc240e77d3e19dc938f00d2a42ce5dc3204bf0fe
Aleksander Avila <aleka.a2001@gmail.com>
2022-11-15 15:38:27 +0300

    second part of first

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,3 @@
 class Task < ApplicationRecord
-  def create_task
-    create_table :Task do |t|
-      t.string :title
-      t.text :details
-      t.boolean :completed, as: false
-    end
-  end
+
 end
